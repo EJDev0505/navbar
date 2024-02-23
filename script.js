a18064a27e9007481fa2b7ddc86233662f22a96a
Ej_Dev <you@example.com>
2024-02-23 15:36:38 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+/*--javascript--*/
+
+const navlinks = document.querySelector("#navlinks");
+
+  function navClose () {
+    navlinks.style.right = "-1220px"
+  } 
+    function navOpen () {
+      navlinks.style.right = "0px"
+  }
